278c05d30bd104a0ad8538c1aba85c553706bb40
Alex Wong Tat Hang <32214395+TopQuark12@users.noreply.github.com>
2018-07-04 21:48:14 +0800

    Base project, init commit
    Ported my airship project base files into power module repo.

---
+++ dev/inc/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: 26 Apr 2018
+ *      Author: Alex's Desktop
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "ch.h"
+#include "hal.h"
+
+#include "MotorPWM.h"
+#include "Bluetooth.h"
+
+#endif /* INC_MAIN_H_ */
